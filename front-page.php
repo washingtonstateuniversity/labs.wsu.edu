02d45e380cc2386470d4ad083ce2ce1457275868
jeremyfelt <jeremy.felt@gmail.com>
2014-11-12 16:19:50 -0800

    Add the beginnings of a front page template

---
+++ front-page.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+	<main>
+
+		<?php get_template_part('parts/headers'); ?>
+
+		<section class="row single gutter marginalize-ends">
+
+			<div class="column one">
+
+			</div><!--/column-->
+
+		</section>
+
+	</main><!--/#page-->
+
+
+<?php get_footer();
