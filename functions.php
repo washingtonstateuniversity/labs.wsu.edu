60cd5ddaf24c39c3bdf879caa4b0a1be8a20ed06
jeremyfelt <jeremy.felt@gmail.com>
2014-11-12 16:21:42 -0800

    Add a display handler to labs theme

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
+include __DIR__ . '/includes/class-labs-display.php';
+
 /**
  * Provide a cache breaking script version for the theme.
  *
---
+++ includes/class-labs-display.php
@@ -0,0 +1,8 @@
+<?php
+
+class Labs_Front_Display {
+	public function __construct() {
+
+	}
+}
+new Labs_Front_Display();
