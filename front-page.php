fe668ee9f4c5ad6fbae6d0e0ada47fd9e68276bf
jeremyfelt <jeremy.felt@gmail.com>
2014-11-12 17:02:51 -0800

    Provide a wonderful, amazing, basic, intial style for 0.0.1 :)

--- front-page.php
+++ front-page.php
@@ -6,19 +6,30 @@
 
 		get_template_part('parts/headers');
 
+		?>
+		<section class="single wide gutter padded-ends">
+			<div class="column one">
+				<h1>Labs at Washington State University</h1>
+			</div>
+		</section>
+		<?php
 		$labs_sites = labs_theme_get_sites();
 		$hc = 0; // horizontal count
 		$hc_class = array( 'one', 'two', 'three' );
+		$bg_class = array( 'palette-a', 'palette-b', 'palette-c', 'palette-d' );
 
 		foreach ( $labs_sites as $lab_site ) {
 			if ( 0 === $hc ) {
-				?><section class="row thirds gutter marginalize-ends"><?php
+				?><section class="row thirds gutter"><?php
 			}
 
-			?><div class="column <?php echo $hc_class[ $hc ]; ?> block-lab-site">
-				<?php echo $lab_site['site_name']; ?>
-
-				<a href="<?php echo esc_url( $lab_site['site_url'] ); ?>">Visit</a>
+			$p = rand( 0, 3 );
+			?><div class="column <?php echo $hc_class[ $hc ]; ?> block-lab-site <?php echo $bg_class[ $p ]; ?>">
+				<a href="<?php echo esc_url( $lab_site['site_url'] ); ?>">
+					<div class="column-internal">
+					<h3><?php echo $lab_site['site_name']; ?></h3>
+					</div>
+				</a>
 			</div><?php
 
 			if ( 2 === $hc ) {
@@ -32,7 +43,7 @@
 		// Close off remaining columns
 		if ( 0 !== $hc ) {
 			while ( $hc <= 2 ) {
-				echo '<div class="column ' . $hc_class[ $hc ] . '"></div>';
+				echo '<div class="column ' . $hc_class[ $hc ] . ' block-lab-site palette-0"></div>';
 				$hc++;
 			}
 			echo '</section>';
