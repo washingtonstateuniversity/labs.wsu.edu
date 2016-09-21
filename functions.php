536fb4a26e1114f45f4bfd3a0a79d09749bb188c
Jeremy Felt <jeremy.felt@gmail.com>
2016-09-21 10:10:02 -0700

    Use the spine_child_theme_version filter to bust cache

--- functions.php
+++ functions.php
@@ -1,12 +1,13 @@
 <?php
 
+add_filter( 'spine_child_theme_version', 'wsu_labs_theme_script_version' );
 /**
- * Provide a cache breaking script version for the theme.
+ * Filter the version number used to enqueue scripts.
  *
- * @return string Current script version
+ * @return string
  */
 function wsu_labs_theme_script_version() {
-	return spine_get_script_version() . '0.0.3';
+	return '0.0.3';
 }
 
 add_action( 'wp_enqueue_scripts', 'wsu_labs_theme_enqueue_scripts', 11 );
@@ -14,5 +15,5 @@ add_action( 'wp_enqueue_scripts', 'wsu_labs_theme_enqueue_scripts', 11 );
  * Enqueue custom Javascript
  */
 function wsu_labs_theme_enqueue_scripts() {
-	wp_enqueue_script( 'wsu-labs-theme', get_stylesheet_directory_uri() . '/js/labs.js', array( 'jquery' ), spine_get_script_version(), true );
+	wp_enqueue_script( 'wsu-labs-theme', get_stylesheet_directory_uri() . '/js/labs.js', array( 'jquery' ), spine_get_child_version(), true );
 }
