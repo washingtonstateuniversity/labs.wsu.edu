33092ad970317df3a3fa91007770146c792de048
Jeremy Felt <jeremy.felt@gmail.com>
2016-09-21 10:10:57 -0700

    Remove unused JavaScript enqueue and file

--- functions.php
+++ functions.php
@@ -1,19 +1,1 @@
 <?php
-
-add_filter( 'spine_child_theme_version', 'wsu_labs_theme_script_version' );
-/**
- * Filter the version number used to enqueue scripts.
- *
- * @return string
- */
-function wsu_labs_theme_script_version() {
-	return '0.0.3';
-}
-
-add_action( 'wp_enqueue_scripts', 'wsu_labs_theme_enqueue_scripts', 11 );
-/**
- * Enqueue custom Javascript
- */
-function wsu_labs_theme_enqueue_scripts() {
-	wp_enqueue_script( 'wsu-labs-theme', get_stylesheet_directory_uri() . '/js/labs.js', array( 'jquery' ), spine_get_child_version(), true );
-}
